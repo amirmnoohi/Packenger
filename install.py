ca8ea7816eadb84cf2dca3c540fd201c9cd1d26f
AMIR MASOUD NOOHI <highlimner@gmail.com>
2020-07-01 13:04:47 -0700

    bug fixes

--- install.py
+++ install.py
@@ -29,10 +29,10 @@ def download_files():
     os.system("clear")
     print(bcolors.OKBLUE + "[*] Downloading Dependencies" + bcolors.ENDC)
     os.system("sudo apt install git gcc g++ make python-dev -y")
-    os.system("sudo rm -rf src/netmap")
-    os.system("sudo git clone https://git.noohi.org/amirmnoohi/netmap src/netmap")
+    os.system("sudo rm -rf lib/netmap")
+    os.system("sudo git clone https://git.noohi.org/amirmnoohi/netmap lib/netmap")
     os.system("clear")
-    if not os.path.exists("src/netmap"):
+    if not os.path.exists("lib/netmap"):
         print(bcolors.OKGREEN + "[+] Changed to Sudo Successfully" + bcolors.ENDC)
         print(bcolors.FAIL + "[-] Failed to Download Dependencies" + bcolors.ENDC)
         exit(0)
@@ -42,8 +42,8 @@ def download_files():
 
 def configure_files():
     print(bcolors.OKBLUE + "[*] Configuring Dependencies" + bcolors.ENDC)
-    os.chdir("src/netmap")
-    os.system("sudo ./src/netmap/configure")
+    os.chdir("lib/netmap")
+    os.system("sudo ./lib/netmap/LINUX/configure --select-version=ixgbe:5.7.1,i40e:2.11.29,e1000e:3.8.4")
     os.chdir(__RUNNING_DIR__)
     os.system("clear")
     print(bcolors.OKGREEN + "[+] Changed to Sudo Successfully" + bcolors.ENDC)
@@ -53,10 +53,10 @@ def configure_files():
 
 def install_files():
     print(bcolors.OKBLUE + "[*] Installing Dependencies" + bcolors.ENDC)
-    os.chdir("src/netmap")
-    os.system("make && make install")
-    os.chdir("extra/python")
-    os.system("make && make install")
+    os.chdir("lib/netmap/LINUX")
+    os.system("make && sudo make install")
+    os.chdir("../extra/python")
+    os.system("make && sudo make install")
     os.chdir(__RUNNING_DIR__)
     os.system("clear")
     print(bcolors.OKGREEN + "[+] Changed to Sudo Successfully" + bcolors.ENDC)
@@ -67,8 +67,8 @@ def install_files():
 
 def load_kernel_module():
     print(bcolors.OKBLUE + "[*] Loading Kernel Module" + bcolors.ENDC)
-    os.system("sudo insmod src/netmap/netmap.ko")
-    os.system("clear")
+    os.system("sudo insmod lib/netmap/LINUX/netmap.ko")
+    # os.system("clear")
     print(bcolors.OKGREEN + "[+] Dependencies Downloaded successfully" + bcolors.ENDC)
     print(bcolors.OKGREEN + "[+] Dependencies Configured successfully" + bcolors.ENDC)
     print(bcolors.OKGREEN + "[+] Dependencies Installed successfully" + bcolors.ENDC)
@@ -76,8 +76,8 @@ def load_kernel_module():
 
 
 def clearing():
-    os.system("sudo cp src/netmap/netmap.ko .")
-    os.system("sudo rm -rf src/netmap")
+    os.system("sudo cp lib/netmap/LINUX/netmap.ko .")
+    os.system("sudo rm -rf lib/netmap")
 
 
 def main():
